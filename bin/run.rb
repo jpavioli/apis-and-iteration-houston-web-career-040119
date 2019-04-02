aa7659de3b61125ebb8b68b6916fdcdc009c4b7d
jpavioli <justin.avioli@gmail.com>
2019-04-02 16:05:06 -0500

    Star Wars API Demo Complete! (04.02.19)

--- bin/run.rb
+++ bin/run.rb
@@ -3,6 +3,55 @@
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
-welcome
-character = get_character_from_user
-show_character_movies(character)
+require 'rest-client'
+require 'json'
+require 'pry'
+
+character_data = RestClient.get('http://swapi.co/api/people/1')
+character_hash = JSON.parse(character_data)
+
+def welcome_fun
+  puts "Welcome to this Star Wars API demo!"
+end
+
+def get_character
+  puts "Please enter a character"
+  character = gets.chomp
+  puts "User Entered #{character}"
+  puts "*" * 30
+  character
+end
+
+def get_films(user_character,character_hash)
+  film_api_list =[]
+  character_hash.find do |key,value|
+    if key == "name" && value == user_character
+      film_api_list << character_hash["films"]
+    end
+  end
+  film_api_list.flatten
+end
+
+def get_film_name(film_api_list)
+  film_api_list.map do |movie|
+    film_hash = JSON.parse(RestClient.get("#{movie}"))
+    film_hash["title"]
+  end
+end
+
+welcome_fun
+user_character = nil
+until user_character == "exit" do
+  user_character = get_character
+  film_api_list = get_films(user_character,character_hash)
+  if film_api_list != []
+    film_list = get_film_name(film_api_list)
+    puts "#{user_character} makes an apearance in the following movies!"
+    puts film_list
+  elsif user_character == "exit"
+    puts "Thanks for using the Star Wars API demo!"
+  else
+    puts "#{user_character} is not in a Star Wars movie!"
+  end
+  puts "*" * 30
+end
